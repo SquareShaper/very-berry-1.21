688b620724261a096e1c370e2cdbea10e32fde94
SquareShaper <squareshaper@tuta.io>
2024-11-27 16:01:34 +0100

    continued skipping

--- src/main/java/net/squareshaper/effect/VoidSkipperEffect.java
+++ src/main/java/net/squareshaper/effect/VoidSkipperEffect.java
@@ -3,10 +3,8 @@ package net.squareshaper.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.Heightmap;
-import net.minecraft.world.gen.HeightContext;
-import net.minecraft.world.gen.YOffset;
 
 public class VoidSkipperEffect extends StatusEffect {
     public VoidSkipperEffect(StatusEffectCategory category, int color) {
@@ -17,12 +15,25 @@ public class VoidSkipperEffect extends StatusEffect {
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
         Vec3d pos = entity.getPos();
         Vec3d vel = entity.getVelocity();
+        double boostSpeed = 5;
+        double amplifierExponent = 2;
 
-        YOffset voidLayer = YOffset.aboveBottom(10);
+        Identifier dimension = entity.getEntityWorld().getRegistryKey().getValue();
 
-        if (pos.getY() <= voidLayer.getY(entity.getWorld().getDimensionEntry().getKeyOrValue().)) {
-            entity.setVelocity(new Vec3d(vel.getX(), 20, vel.getZ()));
+        int voidLayer;
+
+        switch (dimension.toString()) {
+            case "minecraft:overworld":
+                voidLayer = -113;
+            default:
+                voidLayer = -50;
         }
+
+        if (pos.getY() <= voidLayer) {
+            Vec3d newVel = new Vec3d(vel.getX(), boostSpeed + Math.pow(amplifier + 1, amplifierExponent), vel.getZ());
+            entity.setVelocity(newVel);
+        }
+
 
         return super.applyUpdateEffect(entity, amplifier);
     }
