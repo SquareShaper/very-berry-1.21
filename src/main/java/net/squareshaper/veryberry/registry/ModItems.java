bf485783f0b407ef7ba8835968e0a15a952fa5b0
SquareShaper <squareshaper@tuta.io>
2025-01-31 22:33:37 +0100

    organized my item registrations, started adding pastes

--- src/main/java/net/squareshaper/veryberry/registry/ModItems.java
+++ src/main/java/net/squareshaper/veryberry/registry/ModItems.java
@@ -18,15 +18,20 @@ import net.squareshaper.veryberry.item.EffectFoodItem;
 import java.util.List;
 
 public class ModItems {
+    //Berries
     public static final Item RIMEBERRIES = registerItem("rimeberries", new AliasedEffectFoodItem(ModBlocks.RIMEBERRY_BUSH,
             new Item.Settings().food(ModFoodComponents.RIMEBERRIES)));
-
-    public static final Item RIMEBERRY_MUFFIN = registerItem("rimeberry_muffin", new EffectFoodItem(
-            new Item.Settings().food(ModFoodComponents.RIMEBERRY_MUFFIN).recipeRemainder(Items.BUCKET)));
 
     public static final Item FIRESHINE_BERRIES = registerItem("fireshine_berries", new AliasedEffectFoodItem(ModBlocks.FIRESHINE_BERRY_HEAD,
             new Item.Settings().food(ModFoodComponents.FIRESHINE_BERRIES)));
 
+    public static final Item VOIDBERRIES = registerItem("voidberries", new AliasedEffectFoodItem(ModBlocks.VOID_BERRY_MOSS,
+            new Item.Settings().food(ModFoodComponents.VOIDBERRIES)));
+
+    public static final Item CHRONOBERRIES = registerItem("chronoberries", new AliasedEffectFoodItem(Blocks.STRIPPED_BAMBOO_BLOCK,
+            new Item.Settings().food(ModFoodComponents.CHRONOBERRIES)));
+
+    //Berry Foods
     public static final Item FIRESHINE_BERRY_JUICE = registerItem("fireshine_berry_juice", new DrinkItem(
             new Item.Settings().food(ModFoodComponents.FIRESHINE_BERRY_JUICE), Items.GLASS_BOTTLE) {
         @Override
@@ -36,11 +41,14 @@ public class ModItems {
         }
     });
 
-    public static final Item VOIDBERRIES = registerItem("voidberries", new AliasedEffectFoodItem(ModBlocks.VOID_BERRY_MOSS,
-            new Item.Settings().food(ModFoodComponents.VOIDBERRIES)));
+    public static final Item RIMEBERRY_MUFFIN = registerItem("rimeberry_muffin", new EffectFoodItem(
+            new Item.Settings().food(ModFoodComponents.RIMEBERRY_MUFFIN).recipeRemainder(Items.BUCKET)));
 
-    public static final Item CHRONOBERRIES = registerItem("chronoberries", new AliasedEffectFoodItem(Blocks.STRIPPED_BAMBOO_BLOCK,
-            new Item.Settings().food(ModFoodComponents.CHRONOBERRIES)));
+    //Pastes
+    public static final Item FIRESHINE_BERRY_PASTE = registerItem("fireshine_berry_paste", new Item(new Item.Settings()));
+    public static final Item RIMEBERRY_PASTE = registerItem("rimeberry_paste", new Item(new Item.Settings()));
+    public static final Item VOIDBERRY_PASTE = registerItem("voidberry_paste", new Item(new Item.Settings()));
+    public static final Item CHRONOBERRY_PASTE = registerItem("chronoberry_paste", new Item(new Item.Settings()));
 
 
 
