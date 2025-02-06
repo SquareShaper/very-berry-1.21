7b3313cf937d07ac0b0f219007a616b25b134f5d
SquareShaper <anonymoushacker.cheaphacks@gmail.com>
2025-02-06 12:41:09 +0100

    now "branches" grow on cacti - up to 2 branches per block on the cactus. rn they're just candles, adding the branch block later uwu

--- src/main/java/net/squareshaper/veryberry/mixin/thornberry/CactusBlockMixin.java
+++ src/main/java/net/squareshaper/veryberry/mixin/thornberry/CactusBlockMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,9 +24,61 @@ public class CactusBlockMixin extends Block {
     private void veryberry$growBerry(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
         //check if it's the bottom-most cactus block
         if (world.getBlockState(pos.down()).getBlock() != Blocks.CACTUS && !world.isAir(pos.up())) {
-            //set yourself to tnt (for testing purposes)
-            Block blockToSet = Blocks.GREEN_CANDLE;
-            world.setBlockState(pos, blockToSet.getDefaultState());
+            int age = state.get(CactusBlock.AGE);
+            if (age >= 15) {
+                //reset age
+                world.setBlockState(pos, state.with(CactusBlock.AGE, 0), Block.NO_REDRAW);
+
+                //go up the stem, and check the height
+                int i = 1;
+                while (world.getBlockState(pos.up(i)).getBlock() == Blocks.CACTUS) {
+                    i++;
+                }
+
+                //add a branch, in a random direction, and at a random height (not at the bottom tho)
+                Direction[] directions = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+                int direction = world.getRandom().nextInt(3);
+                int height = world.getRandom().nextInt(i - 1) + 1;
+                //first, check if there's a branch at this height already, then only a 10% chance to grow (technically lower, since branches), if there are 2, don't grow
+                int branchSum = 0;
+                if (!world.getBlockState(pos.up(height).north()).isAir()) {
+                    branchSum++;
+                }
+                if (!world.getBlockState(pos.up(height).east()).isAir()) {
+                    branchSum++;
+                }
+                if (!world.getBlockState(pos.up(height).south()).isAir()) {
+                    branchSum++;
+                }
+                if (!world.getBlockState(pos.up(height).west()).isAir()) {
+                    branchSum++;
+                }
+
+                if (branchSum == 0) {
+                    if (world.getBlockState(pos.up(height).offset(directions[direction])).isAir()) {
+                        Block blockToSet = Blocks.GREEN_CANDLE;
+                        world.setBlockState(pos.up(height).offset(directions[direction]), blockToSet.getDefaultState());
+                    }
+                } else if (branchSum == 1) {
+                    if (world.getRandom().nextInt(9) == 0) {
+                        if (world.getBlockState(pos.up(height).offset(directions[direction])).isAir()) {
+                            Block blockToSet = Blocks.GREEN_CANDLE;
+                            world.setBlockState(pos.up(height).offset(directions[direction]), blockToSet.getDefaultState());
+                        }
+                        else {
+                            //try again, since the branch might miss if it hits the branch that was already grown
+                            direction = (direction + 1)%4;
+                            if (world.getBlockState(pos.up(height).offset(directions[direction])).isAir()) {
+                                Block blockToSet = Blocks.GREEN_CANDLE;
+                                world.setBlockState(pos.up(height).offset(directions[direction]), blockToSet.getDefaultState());
+                            }
+                        }
+                    }
+                }
+            } else {
+                //keep aging
+                world.setBlockState(pos, state.with(CactusBlock.AGE, age + 1), Block.NO_REDRAW);
+            }
         }
     }
 }
