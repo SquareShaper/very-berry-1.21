01976c799f70be0a76e220cc1d0047c8c92a2273
SquareShaper <anonymoushacker.cheaphacks@gmail.com>
2025-02-06 19:48:36 +0100

    working on the chronoberry plant, kinda hard ngl borrow more from pitcher plant

--- src/main/java/net/squareshaper/veryberry/block/ChronoberryPlant.java
+++ src/main/java/net/squareshaper/veryberry/block/ChronoberryPlant.java
@@ -1,12 +1,18 @@
 package net.squareshaper.veryberry.block;
 
 import net.minecraft.block.Fertilizable;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.item.Item;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
 import net.squareshaper.veryberry.registry.ModItems;
 
 public class ChronoberryPlant extends TallBerryBlock implements Fertilizable {
+    public static final EnumProperty<DoubleBlockHalf> HALF = Properties.DOUBLE_BLOCK_HALF;
+
     public ChronoberryPlant(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(HALF, DoubleBlockHalf.LOWER));
     }
 
     @Override
